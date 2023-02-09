5f5bcff3bd70e1009e1fb1b9e889bbe4874f0c02
Isa Mesina <isabelmesina@gmail.com>
2023-02-09 14:51:12 -0300

    change index.ts route

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import app from "./app";
-import "./database";
+import app from "./src/app";
+import "./src/database";
 require("dotenv").config();
 
 app.listen(app.get("port"), () => {
--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,8 @@ export default {
     MONGO_HOST: process.env.MONGO_HOST ||'localhost',
     PORT: process.env.PORT || 3001,
     MONGO_URI: process.env.MONGO_URI,
-    MONGO_DEPLOY: process.env.MONGO_DEPLOY
+    MONGO_DEPLOY: process.env.MONGO_DEPLOY,
+    DB_DEPLOY: process.env.DB_DEPLOY
 }
     export const CLOUDINARY_CLOUD_NAME= process.env.CLOUDINARY_CLOUD_NAME;
     export const CLOUDINARY_API_KEY= process.env.CLOUDINARY_API_KEY;
